23b3b8ea54796e5e75ef93428110ea545bcda2fd
TryMeDev <contacttrymenow@gmail.com>
2025-02-07 12:50:10 +0800

    added translations

--- game/bag.gd
+++ game/bag.gd
@@ -7,12 +7,17 @@ var ITEMS = [
 	{"id": 3, "name": "ITEM_3", "effect": {"spirit": 60}, "usable": true},
 	{"id": 4, "name": "ITEM_4", "effect": {}, "usable": false},
 	{"id": 5, "name": "ITEM_5", "effect": {"mood": 65}, "usable": true},
-	{"id": 6, "name": "ITEM_6", "effect": {"hp": 10, "hunger": 10, "spirit": 10, "mood": 10}, "usable": true},
+	{"id": 6, "name": "ITEM_6", "effect": {}, "usable": true},
 	{"id": 7, "name": "ITEM_7", "effect": {}, "usable": false},
 	{"id": 8, "name": "ITEM_8", "effect": {}, "usable": false},
 ]
 
 func use_item(id: int) -> void:
-	var item = ITEMS.filter(func(i): return i.id==id)
-	if item.size()>0:
-		Game.update_status(item[0].effect.merged({"bag": {id: -1}}))
+	if id==6:
+		var new_bag = State.progress.get("bag", {}).duplicate(true)
+		new_bag[6] -= 1
+		State.merge_progress({"hp": randi_range(1,100), "hunger": randi_range(1,100), "spirit": randi_range(1,100), "mood": randi_range(1,100), "bag": new_bag})
+	else:
+		var item = ITEMS.filter(func(i): return i.id==id)
+		if item.size()>0:
+			Game.update_status(item[0].effect.merged({"bag": {id: -1}}))
--- game/sleep.gd
+++ game/sleep.gd
@@ -1,6 +1,9 @@
 class_name Sleep
 
-func sleep() -> bool:
+func sleep(sleeping_pill: bool) -> bool:
+	if sleeping_pill:
+		Game.update_status({"spirit": 50, "hp": 25, "time": 8, "bag": {4: -1}})
+		return true
 	if randf()*100 < State.progress.mood:
 		Game.update_status({"spirit": 50, "hp": 25, "time": 8})
 		return true
